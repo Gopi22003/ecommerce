2e5e884fd0cee422cf657084004e3d47e7615504
gopichandh <gopichandh@gopichandhs-MacBook-Pro.local>
2025-06-09 22:47:46 +0530

    hello

---
+++ index.js
@@ -0,0 +1,9 @@
+//let element = document.querySelectorAll("head")
+//console.log(element)
+// document.body.style.backgroundColor = "red";
+
+
+let p1Ele=document.createElement("h1");
+p1Ele.textContent="Hi how are you";
+p1Ele.style.color="blue";
+document.body.appendChild(p1Ele);
